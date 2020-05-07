e08cabf6861fa7dd7ccd802da82eaf6dfa2f5b21
zhanggh <guanghui.zhang.ext@ai-ways.com>
2020-05-07 15:50:05 +0800

    add a test

--- app/src/main/java/com/ghzhang/colorpickerview/MainActivity.java
+++ app/src/main/java/com/ghzhang/colorpickerview/MainActivity.java
@@ -3,12 +3,22 @@ package com.ghzhang.colorpickerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ColorPickerView colorPickerView = ((ColorPickerView) findViewById(R.id.pickerView));
+        textView = ((TextView) findViewById(R.id.textView));
+        colorPickerView.setOnColorSelectListener(new ColorPickerView.ColorSelectListener() {
+            @Override
+            public void onColorChange(int currentValue) {
+                textView.setTextColor(currentValue);
+            }
+        });
     }
 }
